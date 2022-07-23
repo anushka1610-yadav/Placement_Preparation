abfba551babfd4511c18ad4a851c09023d9f0398
anushka1610-yadav <anushkaydv1610@gmail.com>
2022-07-23 23:43:55 +0530

    added

--- Week_1/Day_7/Max_consecutive_ones.cpp
+++ Week_1/Day_7/Max_consecutive_ones.cpp
@@ -3,23 +3,32 @@ public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int c=0;
         int ans=0;
-        if(nums.size()==0)
-            return 0;
+        // if(nums.size()==0)
+        //     return 0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]!=1)
+        //         continue;
+        //     int j=i+1;
+        //     c=1;
+        //     while(j<nums.size() && nums[j]==1)
+        //     {
+        //         c++;
+        //         j++;
+        //     }
+        //     ans=max(ans,c);
+        //     i=j;
+        // }
+        // return ans;
         for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]!=1)
-                continue;
-            int j=i+1;
-            c=1;
-            while(j<nums.size() && nums[j]==1)
-            {
+            if(nums[i]==1)
                 c++;
-                j++;
-            }
+            else
+                c=0;
+            
             ans=max(ans,c);
-            i=j;
         }
         return ans;
-        
     }
 };
