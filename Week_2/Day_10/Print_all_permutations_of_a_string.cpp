0bcee39503c1e0b889e9db0e1b5fcfc2281ed4ab
anushka1610-yadav <anushkaydv1610@gmail.com>
2022-07-30 12:11:39 +0530

    added

---
+++ Week_2/Day_10/Print_all_permutations_of_a_string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void permute(int ind, vector<int>&nums, vector<vector<int>>&ans)
+    {
+        if(ind==nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=ind;i<nums.size();i++)
+        {
+            swap(nums[ind], nums[i]);
+            permute(ind+1, nums, ans);
+            swap(nums[ind], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        permute(0, nums, ans);
+        return ans;
+        
+    }
+};
